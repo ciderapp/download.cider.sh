31e4044d64e367ffecc6abb05f363cf179273fcb
child_duckling <19170969+quacksire@users.noreply.github.com>
2022-03-01 19:04:05 -0800

    beans

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:ciderapp/download.cider.sh.git master:gh-pages
+cd -
+
+
+
+
